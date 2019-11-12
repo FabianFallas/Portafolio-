3d9a1cd0393a24382f1c1920aaa01c27ae697f86
FabianFallas <37458918+FabianFallas@users.noreply.github.com>
2019-11-12 02:56:49 -0600

    materia
    Seagragan fotos con la materia

---
+++ Portafolio/cmake-build-debug/CMakeFiles/Portafolio.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Portafolio.dir/main.cpp.o"
+  "Portafolio.pdb"
+  "Portafolio"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Portafolio.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Portafolio/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "este es mi portafolios" << std::endl;
+    return 0;
+}
